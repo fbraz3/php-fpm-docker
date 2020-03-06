12651a192cca9a4fa243588a2142c4084e687565
Felipe Keller Braz <fbraz3@gmail.com>
2020-03-06 20:20:09 -0300

    support to set uid and guid

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 $(which chmod) 700 /etc/monit/monitrc
 
-#CLEAR TMP FILES
+# CLEAR TMP FILES
 /root/autoclean.sh
 
-#ADD CRON
+# ADD CRON
 CRONFILE="/cronfile.final"
 SYSTEMCRON="/cronfile.system"
 USERCRON="/cronfile"
@@ -19,7 +19,7 @@ if [ -f "$USERCRON" ]; then
 fi
 /usr/bin/crontab $CRONFILE
 
-#DECLARE/SET VARIABLES
+# DECLARE/SET VARIABLES
 PHPVERSION=`cat /PHP_VERSION 2>/dev/null`
 if [ -z "$PHPVERSION" ]; then
     PHPVERSION=`php -v|grep --only-matching --perl-regexp "7\.\\d+" |head -n1`
@@ -29,7 +29,17 @@ if [ -z "$PHPVERSION" ]; then
     PHPVERSION='7.3'
 fi
 
-#SORRY FOR THAT =(
+# SET UID FOR www-data USER
+if  [[ ! -z "$DATA_UID" ]] && [[ $DATA_UID =~ ^[0-9]+$ ]] ; then
+	$(which usermod) -u $DATA_UID www-data;
+fi
+
+# SET GUID FOR www-data USER
+if  [[ ! -z "$DATA_UID" ]] && [[ $DATA_UID =~ ^[0-9]+$ ]] ; then
+	$(which groupmod) -g $DATA_GUID www-data;
+fi
+
+# SORRY FOR THAT =(
 if [ -f "/etc/php/fpm/php-fpm.conf" ]; then
     $(which cp) -f /etc/php/fpm/php-fpm.conf /etc/php/$PHPVERSION/fpm/php-fpm.conf
 fi
@@ -42,7 +52,7 @@ if [ -f "/etc/php/fpm/pool.d/www.conf" ]; then
     $(which cp) -f /etc/php/fpm/pool.d/www.conf /etc/php/$PHPVERSION/fpm/pool.d/www.conf
 fi
 
-#POPULATE ENV
+# POPULATE ENV
 echo > /etc/php/$PHPVERSION/fpm/env.conf
 for i in `/usr/bin/env`; do
     PARAM=`echo $i |cut -d"=" -f1`
@@ -50,7 +60,7 @@ for i in `/usr/bin/env`; do
     echo "env[$PARAM]=\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/env.conf
 done
 
-#PUPULATE TEMPLATES
+# PUPULATE TEMPLATES
 cp -f /etc/ssmtp/ssmtp.conf.template /etc/ssmtp/ssmtp.conf
 sed -i 's/%MY_HOSTNAME%/'`/bin/hostname`'/g' /etc/ssmtp/ssmtp.conf
 
@@ -59,11 +69,11 @@ $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/php/$PHPVERSION/fpm/pool.
 $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/php/$PHPVERSION/fpm/php-fpm.conf
 
 
-#START SERVICES
+# START SERVICES
 /usr/sbin/service cron restart
 /usr/sbin/service php$PHPVERSION-fpm restart
 sleep 1
 /usr/sbin/service monit start
 
-#KEEP CONTAINER ALIVE
+# KEEP CONTAINER ALIVE
 /usr/bin/tail -f /var/log/php$PHPVERSION-fpm.log
